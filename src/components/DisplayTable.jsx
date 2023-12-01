73b4e6ef758cf331a2748e68615d4140a8be10bf
gyanchith28 <gyanchith@gmail.com>
2023-12-01 16:45:53 +0530

    Display user details in table

--- src/components/DisplayTable.jsx
+++ src/components/DisplayTable.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import UserDataComponent from "./UserData";
 import {
   Table,
   Thead,
@@ -7,17 +6,18 @@ import {
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
 } from "@chakra-ui/react";
+import useFetchData from "./FetchData"; // Adjust the path based on your project structure
 
 export default function DisplayTable() {
+  const users = useFetchData();
+
   return (
-    <TableContainer
-    css= 'padding:60'>
+    <TableContainer css="padding:60">
       <style jsx>{`
         .custom-table tr:nth-child(odd) {
-          background-color: #3081D0;
+          background-color: #3081d0;
         }
 
         .custom-table tr:nth-child(even) {
@@ -34,7 +34,29 @@ export default function DisplayTable() {
           </Tr>
         </Thead>
         <Tbody>
-          <UserDataComponent />
+          {users.map((user) => {
+            const {
+              loan_id,
+              loan_acc_num,
+              fname,
+              mname,
+              lname,
+              loan_start_date,
+              loan_status,
+            } = user;
+            const borrowerName = `${fname} ${mname ? mname + " " : ""}${
+              lname || ""
+            }`;
+
+            return (
+              <Tr key={loan_id}>
+                <Td>{borrowerName}</Td>
+                <Td>{loan_acc_num}</Td>
+                <Td>{loan_start_date}</Td>
+                <Td>{loan_status}</Td>
+              </Tr>
+            );
+          })}
         </Tbody>
       </Table>
     </TableContainer>
