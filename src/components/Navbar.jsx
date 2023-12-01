8d190edeba2f3dc03a2593c4300b31103b42dc3e
gyanchith28 <gyanchith@gmail.com>
2023-12-01 15:07:13 +0530

    add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+// navbar.jsx
+import React from "react";
+import { Box, Flex, Spacer, Text, Icon } from "@chakra-ui/react";
+import { BsPersonCircle } from "react-icons/bs";
+import { MdExitToApp } from "react-icons/md";
 
-export default function Navbar() {
+const Navbar = () => {
+  const text = 'DailyPe';
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <Box bg="#e0f4ff" p={4}>
+      <Flex align="center">
+      <Flex>
+          {text.split('').map((letter, index) => (
+            <Text
+              key={index}
+              fontSize="xl"
+              fontWeight="bold"
+              fontFamily="heading"
+              ml={index !== 0 ? '2px' : '0'} // Add margin only for non-first letters
+            >
+              {letter}
+            </Text>
+          ))}
+        </Flex>
+
+        <Spacer />
+        <Icon as={BsPersonCircle} boxSize={6} color="#333" mr={4} />
+
+        <Text mr={4}>Marg Techno Pvt Ltd</Text>
+
+        <Icon as={MdExitToApp} boxSize={6} color="#333" />
+      </Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
